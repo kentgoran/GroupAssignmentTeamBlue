df8db80cc00397a111397222f43bf3041ced0389
LinneaSmedberg <57094058+LinneaSmedberg@users.noreply.github.com>
2020-09-08 15:17:23 +0200

    Create TestController.cs
    Testing w/ Https

---
+++ GroupAssignmentTeamBlue.Services/Controllers/TestController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroupAssignmentTeamBlue.Services.Controllers
+{
+    public class TestController
+    {
+    }
+}
