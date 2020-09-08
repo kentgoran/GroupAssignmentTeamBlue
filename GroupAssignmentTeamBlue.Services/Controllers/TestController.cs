1de5dfe6b41d532f016826784375abbbb047c591
LinneaSmedberg <57094058+LinneaSmedberg@users.noreply.github.com>
2020-09-08 15:24:52 +0200

    Added testcontroller

--- GroupAssignmentTeamBlue.Services/Controllers/TestController.cs
+++ GroupAssignmentTeamBlue.Services/Controllers/TestController.cs
@@ -1,11 +1,24 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace GroupAssignmentTeamBlue.Services.Controllers
 {
-    public class TestController
+    [ApiController]
+    [Route("api/tests")]
+    public class TestController : ControllerBase
     {
+        private readonly string TestData = "Hello!";
+
+        public TestController()
+        { }
+
+        [HttpGet]
+        public ActionResult GetTestData()
+        {
+            return Ok(TestData);
+        }
     }
 }
