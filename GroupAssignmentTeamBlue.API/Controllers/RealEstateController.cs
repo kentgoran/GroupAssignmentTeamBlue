16c6c983b8b156fd8aebd1a4c1ef48ca6091bd1e
LinneaSmedberg <57094058+LinneaSmedberg@users.noreply.github.com>
2020-09-14 15:22:12 +0200

    Wrote in unit of work prop and fixed get methods for realestate controller

--- GroupAssignmentTeamBlue.API/Controllers/RealEstateController.cs
+++ GroupAssignmentTeamBlue.API/Controllers/RealEstateController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
+using GroupAssignmentTeamBlue.API.Models.DtoModels;
+using GroupAssignmentTeamBlue.DAL.Context;
+using GroupAssignmentTeamBlue.DAL.Repositories;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,24 +17,79 @@ namespace GroupAssignmentTeamBlue.API.Controllers
     public class RealEstateController : ControllerBase
     {
         private readonly IMapper _mapper;
+        private readonly UnitOfWork _unitOfWork;
 
-        public RealEstateController(IMapper mapper)
+        public RealEstateController(IMapper mapper, AdvertContext context)
         {
             _mapper = mapper ?? throw new ArgumentNullException();
+            _unitOfWork = context == null ? 
+                            throw new ArgumentNullException() : new UnitOfWork(context);
         }
 
         [HttpGet]
-        public ActionResult GetRealEstates(int skip = 0, int take = 10)
+        public IActionResult GetRealEstates(int skip = 0, int take = 10)
         {
-            // TODO: Get real estates from repo
-            return NoContent();
+            if (take == 0 || take > 100)
+            {
+                return BadRequest();                
+            }
+
+            var realEstateEntities = _unitOfWork.RealEstateRepository.SkipAndTakeRealEstates(skip, take);
+            var realEstateDtos = _mapper.Map<IEnumerable<RealEstateDto>>(realEstateEntities);
+            return Ok(realEstateDtos);
         }
 
+        // Det här? ...
         [HttpGet("{id}/", Name = "GetRealEstate")]
-        public ActionResult GetRealEstate(int realEstate)
+        public ActionResult GetRealEstate(int realEstateId)
         {
-            // TODO: Get real estate from repo
-            return NoContent();
+            var realEstateEntity = _unitOfWork.RealEstateRepository.Get(realEstateId);
+            if(realEstateEntity == null)
+            {
+                return NotFound();
+            }
+
+            RealEstateDto realEstateDto = null;
+            if (User.Identity.IsAuthenticated)
+            {
+                realEstateDto = _mapper.Map<RealEstatePartlyDetailedDto>(realEstateEntity);
+            }
+            else
+            {
+                realEstateDto = _mapper.Map<RealEstatePartlyDetailedDto>(realEstateEntity);
+            }
+            return Ok(realEstateDto);
         }
+
+
+            // ... eller det här vv ?
+        /*
+        [HttpGet("{id}/", Name = "GetRealEstate")]
+        public ActionResult GetRealEstate(int realEstateId)
+        {
+            var realEstateEntity = _unitOfWork.RealEstateRepository.Get(realEstateId);
+
+            if (realEstateEntity == null)
+            {
+                return NotFound();
+            }
+            var realEstateDto = _mapper.Map<RealEstatePartlyDetailedDto>(realEstateEntity);
+            return Ok(realEstateDto);
+        }
+
+        [Authorize]
+        [HttpGet("{id}/", Name = "GetRealEstate")]
+        public ActionResult GetRealEstateDetails(int realEstateId)
+        {
+            var realEstateEntity = _unitOfWork.RealEstateRepository.Get(realEstateId);
+
+            if(realEstateEntity == null)
+            {
+                return NotFound();
+            }
+            var realEstateDto = _mapper.Map<RealEstateFullDetailDto>(realEstateEntity);
+            return Ok(realEstateDto);
+        }
+        */
     }
 }
