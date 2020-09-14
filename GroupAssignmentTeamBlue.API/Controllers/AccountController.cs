809baa9865fa372bb7a7283026c3017c49504fd0
Simon Westman <simonwestman88@gmail.com>
2020-09-14 10:50:02 +0200

    api/account/register now works

--- GroupAssignmentTeamBlue.API/Controllers/AccountController.cs
+++ GroupAssignmentTeamBlue.API/Controllers/AccountController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using GroupAssignmentTeamBlue.API.Models.DtoModels.ForCreation;
 using GroupAssignmentTeamBlue.Model;
 using Microsoft.AspNetCore.Http;
@@ -15,18 +16,23 @@ namespace GroupAssignmentTeamBlue.API.Controllers
     public class AccountController : ControllerBase
     {
         private readonly UserManager<User> _userManager;
+        private readonly IMapper _mapper;
 
-        public AccountController(UserManager<User> userManager)
+        public AccountController(UserManager<User> userManager, IMapper mapper)
         {
             _userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
+            _mapper = mapper;
         }
         
         [Route("register")]
         [HttpPost]
         [Consumes("application/x-www-form-urlencoded")]
-        public IActionResult RegisterNewUser([FromForm]UserForCreationDto user)
+        public async Task<IActionResult> RegisterNewUser([FromForm]UserForCreationDto userForCreation)
         {
             //TODO: Actually add the new user, hash password etc
+            var user = _mapper.Map<User>(userForCreation);
+            await _userManager.CreateAsync(user);
+            await _userManager.AddPasswordAsync(user, userForCreation.Password);
             return Ok();
         }
     }
