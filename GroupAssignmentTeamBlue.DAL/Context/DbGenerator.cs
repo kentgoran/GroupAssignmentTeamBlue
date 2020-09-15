bf1290dad43e948bfc8aabcf3e01eea2b86fa323
wennstrom <54888925+wennstrom@users.noreply.github.com>
2020-09-15 13:20:10 +0200

    Added Database Seeding

--- GroupAssignmentTeamBlue.DAL/Context/DbGenerator.cs
+++ GroupAssignmentTeamBlue.DAL/Context/DbGenerator.cs
@@ -32,7 +32,6 @@ namespace GroupAssignmentTeamBlue.DAL.Context
         public static void Initializer()
         {
             GenerateRealEstates(5);
-            GenerateRatings();
         }
         private static void GenerateRealEstates(int count)
         {
@@ -77,49 +76,6 @@ namespace GroupAssignmentTeamBlue.DAL.Context
 
                 GeneratedRealEstates.Add(realEstateGenerator);
             }
-        }
-        private static void GenerateRatings()
-        {
-            Randomizer.Seed = new Random(12346);
-            var realEstateGenerator = new Faker<RealEstate>()
-                //Generates a title of 3 words
-                .RuleFor(re => re.Title, f => f.Random.Words(3))
-                .RuleFor(re => re.Description, f => f.Lorem.Text())
-                .RuleFor(re => re.Contact, (f, re) => {
-                    return $"{f.Name.FullName()}, {f.Phone.PhoneNumber()}";
-                })
-                .RuleFor(re => re.User, GenerateUser())
-                .RuleFor(re => re.IsRentable, f => f.Random.Bool())
-                //IsSellable becomes the oposite of IsRentable
-                .RuleFor(re => re.IsSellable, (f, re) =>
-                {
-                    return !re.IsRentable;
-                })
-                //Randomizes field of enum EstateTyoe
-                .RuleFor(re => re.Type, f => f.Random.Enum<EstateType>())
-                .RuleFor(re => re.Rent, (f, re) =>
-                {
-                    return re.IsRentable ? (decimal?)f.Random.Decimal(4000, 15000) : null;
-                })
-                .RuleFor(re => re.SellPrice, (f, re) =>
-                {
-                    return re.IsSellable ? (decimal?)f.Random.Decimal(100000, 2500000) : null;
-                })
-                .RuleFor(re => re.YearBuilt, f => f.Random.Int(1600, 2022))
-                .RuleFor(re => re.DateOfAdvertCreation, (f, re) =>
-                {
-                    return f.Date.Between(Convert.ToDateTime(re.YearBuilt - 01 - 01), Convert.ToDateTime(re.YearBuilt - 01 - 01));
-                })
-                .RuleFor(re => re.Comments, (f, re) =>
-                {
-                    return GenerateComments(re.User, re, f.Random.Int(0, 5));
-                })
-                .RuleFor(re => re.Address, (f, re) =>
-                {
-
-                    return $"{f.Address.FullAddress()}"; ;
-                });
-
         }
         private static User GenerateUser()
         {
