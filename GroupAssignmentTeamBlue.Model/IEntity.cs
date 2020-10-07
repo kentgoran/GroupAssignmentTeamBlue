41bb9cfab3615d7037ae547482430619f22d71d5
LinneaSmedberg <57094058+LinneaSmedberg@users.noreply.github.com>
2020-10-07 18:12:40 +0200

    Added IEntity

---
+++ GroupAssignmentTeamBlue.Model/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GroupAssignmentTeamBlue.Model
+{
+    public interface IEntity
+    {
+        int Id { get;};
+    }
+}
