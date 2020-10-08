2fdda93635ee4acbfeeae05797e1d8a87d8cbfca
LinneaSmedberg <57094058+LinneaSmedberg@users.noreply.github.com>
2020-10-08 18:00:42 +0200

    Added AccountControllerTests Class

---
+++ GroupAssignmentTeamBlue.UnitTests/Controllers/AccountControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using GroupAssignmentTeamBlue.API.Controllers;
+using GroupAssignmentTeamBlue.Model;
+using Microsoft.AspNetCore.Identity;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GroupAssignmentTeamBlue.UnitTests.Controllers
+{
+    public class AccountControllerTests
+    {
+        private readonly AccountController controller;
+
+        public AccountControllerTests()
+        {
+            var userStore = new Mock<IUserStore<User>>();
+            var hasher = new Mock<IPasswordHasher<User>>();
+            var userManager = new Mock<UserManager<User>>(userStore, null, hasher, null, null, null, );
+
+
+            controller = new AccountController();
+        }
+    }
+}
